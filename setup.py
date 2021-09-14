23615ccc0ae9dc6dd0b916851a05cad66603d984
Ville Skyttä <ville.skytta@iki.fi>
2021-09-14 08:08:20 +0300

    refactor(version): move to __init__.py
    release-please now supports it from there without special pyproject.toml
    arrangements.
    
    Refs https://github.com/googleapis/release-please/pull/1026

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup  # type: ignore[import]
 
 def get_version() -> str:
     """Extract version number."""
-    fname = "pytekukko/version.py"
+    fname = "pytekukko/__init__.py"
     with open(fname, encoding="utf-8") as file_:
         for line in file_:
             if line.startswith("__version__"):
